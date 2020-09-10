273dba79e7c6ba84047b61248bfcc42a45184dde
gprok <gprok60@gmail.com>
2020-09-10 13:21:11 +0300

    sring boot basic project created

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PetClinic
-Pet Clinic Web App using the Java Spring framework.
+
+Pet Clinic Web App using the Java Spring Boot framework.
+
+Attending: **Spring Framework 5: Beginner to Guru** at Udemy, by John Thompson
---
+++ src/test/java/gprok/springframework/petclinic/PetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package gprok.springframework.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
